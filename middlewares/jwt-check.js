148368ebb37378e74c499a96a1f926c9071f1aa9
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-07-06 22:48:59 +0300

    removal of localhost proxy

--- middlewares/jwt-check.js
+++ middlewares/jwt-check.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 function jwtCheck(secret) {
     return function (req, res, next) {
       const { token } = req.query;
-
+      console.log('jwt-check.js, token = ', token);
       jwt.verify(token, secret);
 
       next();
